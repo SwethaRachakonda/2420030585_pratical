d75646dbdbabd382e61b166862b12d8d8a97b11a
SwethaRachakonda <swetharachakonda9@gmail.com>
2025-08-05 11:05:05 +0530

    This is typeconversion.java file

---
+++ manualcast.java
@@ -0,0 +1,8 @@
+public class manualcast {
+    public static void main(String[] args) {
+        double myDouble = 9.78;
+        int myInt = (int) myDouble;
+        System.out.println(myDouble);
+        System.out.println(myInt);
+    }
+}
