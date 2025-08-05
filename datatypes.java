0e940dd22a13a9886792f721d4d40562886c546f
SwethaRachakonda <swetharachakonda9@gmail.com>
2025-08-05 10:46:49 +0530

    This is datatypes.java file

---
+++ datatypes.java
@@ -0,0 +1,16 @@
+public class datatypes {
+    public static void main(String args[]) {
+        int a = 10;
+        float b = 20.6F;
+        double d = 35.65;
+        String S = "how are you";
+        char c = 'A';
+        boolean e = true;
+        System.out.println("int" + a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(S);
+    }
+}
